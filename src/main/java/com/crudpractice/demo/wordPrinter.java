e65632cff13e9b7648bd7fcfb6d2580019186c2b
Amina <srna.amina24@gmail.com>
2025-08-12 14:15:54 +0200

    initial commit

---
+++ src/main/java/com/crudpractice/demo/wordPrinter.java
@@ -0,0 +1,9 @@
+package com.crudpractice.demo;
+
+public class wordPrinter {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
+
---
+++ src/test/java/com/crudpractice/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudpractice.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
